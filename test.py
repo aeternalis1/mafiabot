9a9259f4b22be125071e89d5fd84dac9df6496aa
Michael Huang <michaelhuang88@Outlook.com>
2019-07-01 22:25:21 -0400

    added functionality for toggling settings

--- test.py
+++ test.py
@@ -10,20 +10,30 @@ async def on_ready():
 
 
 help_text = [
-"```List of commands```",
-"Type t!help [command] to receive details about the command itself.",
-"**1. Basic**: `help` `h2p` `start` `end`",
-    # refers to itself, how to play, start game, end game (only mod or player)
-"**2. Setup**: `roles` `add` `remove` `setup` `settings` `toggle`",
-    # see all roles, add/remove role, check current setup
-"**3. In-game**: `vote` `unvote` `status` `players` `alive`"
-    # vote [player], unvote, who is voting whom, alive players, alive roles
+    "```List of commands```",
+    "Type t!help [command] to receive details about the command itself.",
+    "**1. Basic**: `help` `h2p` `start` `end`",
+        # refers to itself, how to play, start game, end game (only mod or player)
+    "**2. Setup**: `roles` `add` `remove` `setup` `settings` `toggle` `join` `leave`",
+        # see all roles, add/remove role, check current setup
+    "**3. In-game**: `vote` `unvote` `status` `players` `alive`"
+        # vote [player], unvote, who is voting whom, alive players, alive roles
 ]
 
+h2p_text = [
+    "**How to play:**",
+    "Mafia is a party game in which all the players are split into two opposing factions: the innocent villagers and the guilty mafia.\n",
+    "The game alternates between two phases:",
+    "1. Daytime, when players can discuss and debate the identity of the mafia. Players can also majority vote to lynch one member of the community who they suspect of being guilty.",
+    "2. Nighttime, when mafia are free to murder one innocent citizen of the town, and certain townspeople can use their special abilities.\n",
+    "If you are a villager, your win condition is to identify and lynch all of the mafia.",
+    "If you are a mafia, your win condition is to either equal or outnumber the townspeople.",
+    "At the start of the game, your role will be assigned to you via DM by this bot."
+]
 
 commands = {
-    'help': '`m!help` displays help screen. Fairly obvious.',
-    'h2p': '`m!h2p` describes basic rules and premise of the game.',
+    'help': '`m!help` displays the help screen. Fairly obvious.',
+    'h2p': '`m!h2p` describes the basic rules and premise of the game.',
     'start': '`m!start` begins a new round of mafia.',
     'end': '`m!end` ends the current game, if existing. Can only be called by a moderator or a player.',
     'roles': '`m!roles` lists all available roles that can be added to the game.',
@@ -32,6 +42,8 @@ commands = {
     'setup': '`m!setup` shows the full complement of roles in the current setup.',
     'settings': '`m!settings` displays all the settings of the current game.',
     'toggle': '`m!toggle [setting]` flips `[setting]` from true to false, or vice versa. Do `m!settings` to see options',
+    'join' : '`m!join` makes you join the game.',
+    'leave' : '`m!leave` makes you leave the game.',
     'vote': '`m!vote [player]` puts your current vote on `player`.',
     'unvote': '`m!unvote` sets your vote to nobody (no vote).',
     'status': '`m!status` displays all players and their votes, as well as current voting leaders.',
@@ -40,20 +52,43 @@ commands = {
 }
 
 
+settings = {
+    'daystart': 0,      # game starts during daytime
+    'selfsave': 0,      # doctor can save themselves
+    'conssave': 0,      # doctor can save the same person in consecutive turns
+    'paritycop': 0,     # parity cop if true (gets reports on if people are aligned the same way)
+    'limit1': 'inf',    # time limit for days
+    'limit2': 'inf'     # time limit for nights
+}
+
+
+toggle_text = [{
+    'daystart': 'daystart toggled off: The game will commence during nighttime.',
+    'selfsave': 'selfsave toggled off: The doctor will not be able to save himself during nighttime.',
+    'conssave': 'conssave toggled off: The doctor will not be able to save the same patient over consecutive nights.',
+    'paritycop': 'paritycop toggled off: The cop will receive a report stating the alignment of the target (innocent or guilty)'
+}, {
+    'daystart': 'daystart toggled on: The game will commence during daytime.',
+    'selfsave': 'selfsave toggled on: The doctor will be able to save himself during nighttime.',
+    'conssave': 'conssave toggled on: The doctor will be able to save the same patient over consecutive nights.',
+    'paritycop': 'paritycop toggled on: The cop will receive a report stating if his LAST TWO targets are of the same alignment or not.'
+}]
+
+
 async def invalid(message):
     await message.channel.send('Invalid request. Please refer to `m!help` for aid.')
 
 async def m_help(message):
     query = message.content.split()
     if len(query) == 1:
-        await message.channel.send("\n".join(help_text))
+        await message.channel.send('\n'.join(help_text))
     elif len(query) == 2 and query[1] in commands:
         await message.channel.send(commands[query[1]])
     else:
         await invalid(message)
 
 async def m_h2p(message):
-    pass
+    await message.channel.send('\n'.join(h2p_text))
 
 async def m_start(message):
     pass
@@ -74,9 +109,37 @@ async def m_setup(message):
     pass
 
 async def m_settings(message):
-    pass
+    await message.channel.send('\n'.join([key+str(": ")+str(settings[key]) for key in settings]))
 
 async def m_toggle(message):
+    query = message.content.split()
+    if query[1] in settings:
+        if query[1] == 'limit1' or query[1] == 'limit2':
+            if query[2] == 'inf':
+                settings[query[1]] = query[2]
+                if query[1] == 'limit1':
+                    await message.channel.send('Time limit for day set to infinite minutes.')
+                else:
+                    await message.channel.send('Time limit for night set to infinite minutes.')
+            else:
+                try:
+                    settings[query[1]] = float(query[2])
+                    if query[1] == 'limit1':
+                        await message.channel.send('Time limit for day set to '+query[2]+' minutes.')
+                    else:
+                        await message.channel.send('Time limit for night set to '+query[2]+' minutes.')
+                except:
+                    await invalid(message)
+        else:
+            settings[query[1]] ^= 1
+            await message.channel.send(toggle_text[settings[query[1]]][query[1]])
+        return
+    await invalid(message)
+
+async def m_join(message):
+    pass
+
+async def m_leave(message):
     pass
 
 async def m_vote(message):
